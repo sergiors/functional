cc80da1eb8c68598e352a78d4b1edf9a54e8a00d
Sérgio Rafael Siqueira <sergio@inbep.com.br>
2016-04-15 20:47:27 -0300

    Update vars

--- src/every.php
+++ src/every.php
@@ -2,13 +2,13 @@
 
 function every($fn, $ls)
 {
-    $ks = array_keys($ls);
-    $ps = (new \ReflectionFunction($fn))->getParameters();
+    $keys = array_keys($ls);
+    $params = (new \ReflectionFunction($fn))->getParameters();
 
-    return array_reduce($ks, function ($carry, $idx) use ($fn, $ls, $ps) {
+    return array_reduce($keys, function ($carry, $idx) use ($fn, $ls, $params) {
         $args = $ls[$idx];
 
-        if (isset($ps[1])) {
+        if (isset($params[1])) {
             $carry[$idx] = call_user_func_array($fn, [$idx, $args]);
             return $carry;
         }
