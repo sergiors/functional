87e16b8e13fe2c170393d596f529145855a525cb
Sérgio Rafael Siqueira <sergio@inbep.com.br>
2016-10-31 18:22:47 -0200

    Improve find

--- src/find.php
+++ src/find.php
@@ -4,26 +4,13 @@ namespace Sergiors\Functional;
 
 const find = __NAMESPACE__.'\find';
 
-/**
- * @author Marcelo Camargo <marcelocamargo@linuxmail.org>
- * @author Sérgio Rafael Siqueira <sergio@inbep.com.br>
- *
- * @param array ...$args
- *
- * @return mixed
- */
 function find(...$args)
 {
-    return partial(function (callable $pred, $xss) {
-        if ([] == $xss) {
-            return null;
-        };
-
-        $x = head($xss);
-        $xs = tail($xss);
-
-        return $pred($x)
-            ? $x
-            : find($pred, $xs);
+    return partial(function (callable $pred, array $xss) {
+        return ifelse(equals([]), always(null), function (array $xss) use ($pred) {
+            $x = head($xss);
+            $xs = tail($xss);
+            return ifelse($pred, id, always(find($pred, $xs)))($x);
+        })($xss);
     })(...$args);
 }
